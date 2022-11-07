030b31c600db2f6c1cec61f86d68c5ccd38d6921
zohaibalimemonx <zohaibalimemonx@gmail.com>
2022-11-07 21:48:28 +0500

    07/11/2022

--- WordPress Quick Code/JS Code/javascript-href.js
+++ WordPress Quick Code/JS Code/javascript-href.js
@@ -1,3 +1,5 @@
+// READ - https://stackoverflow.com/questions/1865837/whats-the-difference-between-window-location-and-window-location-replace
+
 onclick="window.location.href = '#'"
 
 window.location.href = '';
--- WordPress Quick Code/PHP Code/create_shortcode_wordpress.php
+++ WordPress Quick Code/PHP Code/create_shortcode_wordpress.php
@@ -199,4 +199,15 @@
     add_shortcode( 'get_product_variations', 'get_product_variations_callback' );
     // [get_product_variations product_id=""]
 
+    // Add Custom Template Into The Static Page
+    function cs_template_shortcode_wp( $atts ) {
+        $atts = shortcode_atts( array(
+            'page' => ''
+        ), $atts );
+        ob_start();
+        get_template_part($atts['page']);
+        return ob_get_clean();
+    }
+    add_shortcode( 'cs-template-name','cs_template_shortcode_wp' );
+    // [cs-template-name page="template/post-insights"]
 ?>
